3cb2348d1b8b43135cef139dea587be9d959696a
Kevin Li <kyl27@cornell.edu>
2016-05-25 00:29:51 -0400

    Working prototype with numeric directory depth

--- htree.hs
+++ htree.hs
@@ -1,21 +1,21 @@
+import Data.List
 import System.Directory
+
+
+tree :: Int -> FilePath -> IO ()
+tree depth filepath = do
+    contents <- getDirectoryContents filepath
+    let items = filter (not . isPrefixOf ".") contents
+    let fullPaths = map ((filepath ++ "/") ++) items
+    let ls_rec acc ele = do 
+        isFile <- doesFileExist ele
+        if isFile then 
+            acc >> putStrLn (show depth ++ ele)
+        else 
+            acc >> withCurrentDirectory ele (tree (depth + 1) ele)
+    foldl ls_rec (return ()) fullPaths
+
 
 main :: IO ()
 main = do 
-    getCurrentDirectory >>= ls
-
-
-ls :: FilePath -> IO ()
-ls filepath = do
-    -- print filepath
-    contents <- getDirectoryContents filepath
-    let items = filter (`notElem` [".", ".."]) contents
-    let fullPaths = map ((filepath ++ "/") ++) items
-    let ls_rec acc ele = do 
-        print ele
-        isFile <- doesFileExist ele
-        if isFile then 
-            acc 
-        else 
-            withCurrentDirectory ele (ls ele)
-    foldl ls_rec (print "") fullPaths
+    getCurrentDirectory >>= tree 0
