e7cd0b84b2b0c50e4c02258a3e1e114cd502121e
Kevin Li <kyl27@cornell.edu>
2016-05-27 01:28:57 -0400

    Working prototype for header includes

--- htree.hs
+++ htree.hs
@@ -1,5 +1,30 @@
 import Data.List
 import System.Directory
+import System.FilePath
+
+
+getHeaders :: FilePath -> IO (Maybe [FilePath])
+getHeaders filepath = do
+    exists <- doesFileExist filepath
+    if exists then do
+        contents <- readFile filepath
+        return (Just (lines contents))
+    else
+        return Nothing
+
+
+htree :: Int -> FilePath -> IO ()
+htree depth filepath = do
+    headers <- getHeaders filepath
+    case headers of
+        Nothing -> return ()
+        Just files -> foldl include (return ()) files
+        where
+            include acc ele = do
+                cwd <- getCurrentDirectory
+                acc >> putStrLn (indent depth ++ ele)
+                    >> htree (depth + 1) (cwd </> ele)
+
 
 indent :: Int -> String
 indent depth
@@ -13,7 +38,7 @@ tree depth filepath = do
     contents <- getDirectoryContents filepath
     let items = filter (not . isPrefixOf ".") contents
     let ls_rec acc ele = do
-        let fullPath = filepath ++ "/" ++ ele
+        let fullPath = filepath </> ele
         isFile <- doesFileExist fullPath
         if isFile then
             acc >> putStrLn (indent depth ++ ele)
@@ -25,4 +50,5 @@ tree depth filepath = do
 
 main :: IO ()
 main = do
-    getCurrentDirectory >>= tree 0
+    -- getCurrentDirectory >>= tree 0
+    htree 0 "/Users/kevin/workspace/htree/src/file.a"
---
+++ src/file.c
@@ -0,0 +1,3 @@
+file.b
+file.d
+
